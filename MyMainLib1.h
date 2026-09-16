5f9ea0eaa8d98182e59d1c66585580bb19a82fa6
Ahmed Nahad-Hussein <a7med10nbo@gmail.com>
2026-09-16 13:19:36 +0300

    Delete MyMainLib1.h

--- MyMainLib1.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <iostream>
-#include "MyInputLib.h";
-#include "MyPrintLib.h";
-#include "MyGenerateLib.h";
-using namespace std;
-
-//One library including Other libraries We Created
