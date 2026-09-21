2b3f0c7b16c6096c60cc4c370dbbfd800937f9e2
Ahmed Nahad-Hussein <a7med10nbo@gmail.com>
2026-09-21 21:55:40 +0300

    Add files via upload

---
+++ C++ Level 2 Exercises/2-H(Advanced)-Check-IsABalindromeMatrix.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+const short Rows = 3;
+const short Cols = 3;
+
+short GenerateRandomNumbers(short from, short to)
+{
+	return rand() % (to - from + 1) + from;
+}
+
+void FillMatrixByUser(short Matrix[Rows][Cols])
+{
+	cout << "Enter Elements to The Matrix :\n";
+	for (short i = 0; i < Rows; i++)
+	{
+		for (short j = 0; j < Cols; j++)
+		{
+			cin >> Matrix[i][j];
+		}
+	}
+}
+
+void PrintMatrix(short Matrix[Rows][Cols])
+{
+	for (short i = 0; i < Rows; i++)
+	{
+		for (short j = 0; j < Cols; j++)
+		{
+			cout << setw(2) << Matrix[i][j] << "  ";
+		}
+		cout << endl;
+	}
+}
+
+bool IsBalindromeMatrix(short Matr[Rows][Cols])
+{
+	for (short i = 0; i < Rows; i++)
+	{
+		for (short j = 0; j < Cols; j++)
+		{
+			if (Matr[i][j] != Matr[i][Cols - 1 - j])
+				return false;
+		}
+	}
+	return true;
+}
+
+int main()
+{
+	short Matrix[Rows][Cols];
+
+	FillMatrixByUser(Matrix);
+	cout << "\nThe Matrix is :\n";
+	PrintMatrix(Matrix);
+
+	if (IsBalindromeMatrix(Matrix))
+		cout << "\nYes: it is a Balindrome Matrix" << endl;
+	else
+		cout << "\nNo: it is Not a Balindrome Matrix" << endl;
+
+
+	system("pause>0");
+
+	return 0;
+}
