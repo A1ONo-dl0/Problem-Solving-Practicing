2643126bdaddd866a9c2a293a6b872ac1b220598
Ahmed Nahad-Hussein <a7med10nbo@gmail.com>
2026-09-25 13:21:11 +0300

    Add files via upload

---
+++ My-Custom-Libraries/MyMainLib1.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <iostream>
+#include "MyInputLib.h";
+#include "MyPrintLib.h";
+#include "MyGenerateLib.h";
+#include "MyInvert-CheckLib.h";
+using namespace std;
+
+//One library including Other libraries We Created
---
+++ My-Custom-Libraries/TryPracties-CreateLibraries.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <cctype>
+#include "MyMainLib1.h";
+using namespace std;
+
+
+
+int main()
+{
+	srand((unsigned)time(NULL));
+
+	
+	system("pause>0");
+
+	return 0;
+}
