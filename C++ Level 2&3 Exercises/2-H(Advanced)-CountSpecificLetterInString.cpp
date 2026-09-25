c89c1430a6c48c12b8d499c432443efdfa37441f
Ahmed Nahad-Hussein <a7med10nbo@gmail.com>
2026-09-25 13:05:55 +0300

    Add files via upload

---
+++ C++ Level 2&3 Exercises/2-H(Advanced)-CountSpecificLetterInString.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <string>
+#include <cctype>
+using namespace std;
+
+string ReadString()
+{
+	string str;
+	cout << "Enter a String?\n";
+	cout << "Enter: ";
+	getline(cin, str);
+
+	return str;
+}
+
+char UserInputChar(const string& message)
+{
+	char c;
+
+	do
+	{
+		cout << message << "\n";
+		cout << "Enter: ";
+		cin >> c;
+
+		if (isdigit(c))
+		{
+			cout << "\nInvalied Input!, Please Enter a Valied One\n\n";
+		}
+	} while (isdigit(c));
+
+	return c;
+}
+
+short CountLetter(string& str, char& Letter)
+{
+	short Counter = 0;
+
+	for (short i = 0; i < str.length(); i++)
+	{
+		if (str[i] == Letter)
+			Counter++;
+	}
+
+	return Counter;
+}
+
+
+int main()
+{
+	string str = ReadString();
+
+	char c = UserInputChar("\nEnter a Character To Count?");
+
+	cout << "\nLetter '" << c << "' Count = " << CountLetter(str, c) << endl;
+
+	system("pause>0");
+
+	return 0;
+}
